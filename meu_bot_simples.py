c29778c59443df3969758a863a398dfd2b32f4f1
Adriele <83988106+obtuosa@users.noreply.github.com>
2023-04-23 12:17:02 -0300

    Add files via upload

---
+++ meu_bot_simples.py
@@ -0,0 +1,29 @@
+
+import discord
+import os
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.members = True
+intents.message_content = True
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        if message.content == 'regras':
+            await message.channel.send(f'Zzzzzzummmmmmmm, {message.author.name}, o servidor tem Zzzzzzummmmmmmm apenas uma regra: {os.linesep}  Não Zzzzzzummmmmmmm desrespeitar ninguém {os.linesep}  De resto, seja livre neste pequeno Zzzzzzummmmmmmm espaço.')
+        elif message.content == 'nivel':
+            await message.author.send(' Nível de Zzzzzzummmmmmmm intimidade: 1 ')
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            mensagem = f'{member.mention}, lindo (a) gostoso (a) acabou de entrar em {guild.name}'
+            await guild.system_channel.send(mensagem)
+
+client =  MyClient(intents=intents)
+client.run('MTA5ODc3NDIwMzYyMjI0NDQ2NQ.Go82VY.Vxl-AdIArSwDU6jiyeOqQb2b5oG_iSNtETup90')
